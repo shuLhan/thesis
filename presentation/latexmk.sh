568debe11c06f80bb685db819ec2e314bafa4d42
Mhd Sulhan <ms@kilabit.info>
2016-06-08 21:15:45 +0700

    presentation: fix latex make script.

--- presentation/latexmk.sh
+++ presentation/latexmk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-latexmk -pvc -view=pdf
-Muhamad_Sulhan_-_Detecting_Vandalism_on_Wikipedia_with_CRF_presentation.tex
+latexmk -pvc -view=pdf \
+	Muhamad_Sulhan_-_Detecting_Vandalism_on_Wikipedia_with_CRF_presentation.tex
